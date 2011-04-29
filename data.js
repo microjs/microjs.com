4fa6194e0adf11a3a6b5dca07f5497f8b7d3de2b
Thomas Fuchs <thomas@fesch.at>
2011-04-29 11:36:31 -0400

    moar

--- data.js
+++ data.js
@@ -1,4 +1,11 @@
 var MicroJS = [
+  {
+    name: "Modernizr",
+    size: "3.7k",
+    tags: ["feature"],
+    description: "Detects native CSS3 and HTML5 features available in the current browser.",
+    url: "http://www.modernizr.com/"
+  },
   { 
     name: "Zepto",
     size: "4.2k",
@@ -98,6 +105,13 @@ var MicroJS = [
     url: "http://ejohn.org/blog/javascript-micro-templating/"
   },
   {
+    name: "Tweet-Templ",
+    size: "0.1k",
+    tags: ["templating"],
+    description: "function t(s,d){for(var p in d)s=s.replace(new RegExp('{'+p+'}','g'),d[p]);return s;}",
+    url: "http://mir.aculo.us/2011/03/09/little-helpers-a-tweet-sized-javascript-templating-engine/"
+  },
+  {
     name: "Firmin",
     size: "2.5k",
     tags: ["animation","webkit"],
@@ -122,14 +136,42 @@ var MicroJS = [
     name: "$script.js",
     size: "0.8k",
     tags: ["loader"],
-    description: "An asynchronous JavaScript loader and dependency manager with an astonishingly impressive lightweight footprint.",
+    description: "An asynchronous JavaScript loader and dependency manager with an astonishingly impressive footprint.",
     url: "https://github.com/ded/script.js"
   },
   {
     name: "Klass.js",
     size: "0.6k",
     tags: ["language", "class"],
-    descrption: "An expressive, cross platform JavaScript Class provider with a slick, classical interface to prototypal inheritance.",
+    description: "An expressive, cross platform JavaScript Class provider with a slick, classical interface to prototypal inheritance.",
     url: "https://github.com/ded/klass"
+  },
+  {
+    name: "snack.js",
+    size: "3.3k",
+    tags: ["base", "dom", "events", "ajax", "language"],
+    description: "A tiny, cross-browser, library with DOM manipulation, events, pub/sub, Ajax and language utilities.",
+    url: "http://snackjs.com/"
+  },
+  {
+    name: "Ender",
+    size: "10k",
+    tags: ["base", "dom", "events", "ajax", "animation", "language"],
+    description: "A open, powerful, micro-to-macro API for composing your own custom JavaScript library. The 'Jeesh' contains a pre-packaged selection of micro-libs.",
+    url: "http://ender.no.de/"
+  },
+  {
+    name: "Mold",
+    size: "1.9k",
+    tags: ["templating"],
+    description: "Mold is a very light-weight (200 lines) client-side JavaScript-based templating library.",
+    url: "http://marijnhaverbeke.nl/mold/"
+  },
+  {
+    name: "Tempo",
+    size: "3.2k",
+    tags: ["templating"],
+    description: "Tempo is a JSON rendering engine that enables you to craft data templates in pure HTML.",
+    url: "http://twigkit.github.com/tempo/"
   }
 ];
