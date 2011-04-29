00ebac0b9611af9ab7ac68ae42c5494786acda2e
Thomas Fuchs <thomas@fesch.at>
2011-04-29 09:27:06 -0400

    initial commit

---
+++ data.js
@@ -0,0 +1,135 @@
+var MicroJS = [
+  { 
+    name: "Zepto",
+    size: "4.2k",
+    tags: ["base", "dom", "webkit", "jquery", "ajax", "events", "mobile"],
+    description: "jQuery API-compatible micro-framework for mobile WebKit browsers, with DOM manipulation, Ajax and events.",
+    url: "http://zeptojs.com/"
+  },
+  {
+    name: "xui",
+    size: "4.1k",
+    tags: ["base", "dom", "mobile", "ajax", "events", "webkit", "animation"],
+    description: "A super micro tiny dom library for authoring HTML5 mobile web applications, works cross-device and cross-platform.",
+    url: "http://xuijs.com/"
+  },
+  { 
+    name: "Underscore",
+    size: "3k",
+    tags: ["functional", "language", "data"],
+    description: "A utility-belt that provides functional programming support that you would expect in Ruby.",
+    url: "http://documentcloud.github.com/underscore/"
+  },
+  { 
+    name: "Backbone",
+    size: "3.9k",
+    tags: ["mvc", "data", "functional"],
+    description: "Lightweight MVC—models with custom events, collections with rich enumerables, views and RESTful JSON.",
+    url: "http://documentcloud.github.com/backbone/"
+  },
+  {
+    name: "Mustache",
+    size: "1.5k",
+    tags: ["templating"],
+    description: "Minimal, logic-less templating with {{mustaches}}. Great for server- or client-side templating.",
+    url: "http://mustache.github.com/"
+  },
+  {
+    name: "LABjs",
+    size: "2.1k",
+    tags: ["loader"],
+    description: "Flexible and light-weight JavaScript dependency loader.",
+    url: "http://labjs.com/"
+  },
+  {
+    name: "RequireJS",
+    size: "5.5k",
+    tags: ["loader"],
+    description: "File and module loader with extensive optimization options.",
+    url: "http://requirejs.org/"
+  },
+  {
+    name: "Qwery",
+    size: "1k",
+    tags: ["base", "dom"],
+    description: "Qwery is a 1k blazing fast query selector engine allowing you to select elements with CSS1 & CSS2 selectors.",
+    url: "https://github.com/ded/qwery"
+  },
+  {
+    name: "Émile",
+    size: "0.9k",
+    tags: ["animation"],
+    description: "Émile is a no-frills stand-alone CSS animation JavaScript framework, named after Émile Cohl, early animator.",
+    url: "https://github.com/madrobby/emile"
+  },
+  { 
+    name: "Raphaël",
+    size: "20k",
+    tags: ["graphics", "vector", "SVG"],
+    description: "Raphaël is library that simplifies work with vector graphics on the web, supporting SVG and VML (for IE).",
+    url: "http://raphaeljs.com/"
+  },
+  {
+    name: "Lawnchair",
+    size: "2k",
+    tags: ["data","storage","webkit"],
+    description: "Sorta like a couch except smaller and outside, also, a client side JSON document store. Perfect for WebKit mobile apps.",
+    url: "http://westcoastlogic.com/lawnchair/"
+  },
+  {
+    name: "ExplorerCanvas",
+    size: "4.3k",
+    tags: ["polyfill"],
+    description: "HTML5 canvas tag support for Internet Explorer.",
+    url: "http://code.google.com/p/explorercanvas/"
+  },
+  { 
+    name: "Placeholder.js",
+    size: "0.4k",
+    tags: ["polyfill"],
+    description: "Adds support for the placeholder attribute in older browsers that don't support this HTML5 feature.",
+    url: "https://github.com/NV/placeholder.js"
+  },
+  {
+    name: "Micro-Templating",
+    size: "0.2k",
+    tags: ["templating"],
+    description: "Templating function that is fast, caches quickly, and is easy to use.",
+    url: "http://ejohn.org/blog/javascript-micro-templating/"
+  },
+  {
+    name: "Firmin",
+    size: "2.5k",
+    tags: ["animation","webkit"],
+    description: "Animation library that uses CSS transforms and transitions to create smooth, hardware-accelerated animations.",
+    url: "http://extralogical.net/projects/firmin/"
+  },
+  {
+    name: "Bonzo",
+    size: "1.4k",
+    tags: ["dom"],
+    description: "A simple, to the point, hassle-free, small, library agnostic, extensible DOM utility.",
+    url: "https://github.com/ded/bonzo"
+  },
+  {
+    name: "Bean",
+    size: "2.4k",
+    tags: ["events", "dom"],
+    description: "Bean is a small, slick, cross-platform, framework-agnostic event utility designed for desktop, mobile, and touch-based browsers. ",
+    url: "https://github.com/fat/bean"
+  },
+  {
+    name: "$script.js",
+    size: "0.8k",
+    tags: ["loader"],
+    description: "An asynchronous JavaScript loader and dependency manager with an astonishingly impressive lightweight footprint.",
+    url: "https://github.com/ded/script.js"
+  },
+  {
+    name: "Klass.js",
+    size: "0.6k",
+    tags: ["language", "class"],
+    descrption: "An expressive, cross platform JavaScript Class provider with a slick, classical interface to prototypal inheritance.",
+    url: "https://github.com/ded/klass"
+  }
+];
