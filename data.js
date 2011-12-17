a639284bfc46d7298084d37e9a64006801393932
Rod Vagg <rod@vagg.org>
2011-12-17 22:54:27 +1100

    docfix

--- data.js
+++ data.js
@@ -1,3 +1,12 @@
+// See the README (https://github.com/madrobby/microjs.com#readme) for details about how
+// to use this file.
+// Make sure you run 'make compile' to check that your library doesn't come up with
+// warnings or errors. It would also be appreciated if you could check any versioned
+// libraries to see if they have new releases.
+// As per the README, the source should be a link to your unminified, raw, source. It can
+// also be just the raw JS if it's small enough, an array of source files or a ZIP file.
+// See examples below.
+
 var MicroJS = [
   {
     name: "TinyDOM",
