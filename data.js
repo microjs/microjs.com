eac02d6b875019b6897ec38adf5e04b8ccc7eae7
Adrian Miu <florin_miu@yahoo.com>
2013-08-30 16:46:31 +0300

    Added my callbacks library

--- data.js
+++ data.js
@@ -2880,5 +2880,12 @@ module.exports = [
     description: "Promises/A+ micro library to help with asynchronous work flow.",
     url: "https://github.com/avoidwork/assure",
     source: "https://raw.github.com/avoidwork/assure/master/lib/assure.js"
+  },
+  {
+    name: "callbacks.js",
+    tags: ["callbacks", "events", "event manager"],
+    description: "Callbacks library similar to jQuery's Callbacks. There's also an event manager that allows you to use on(), one(), off() and trigger()",
+    url: "https://github.com/adrianmiu/callbacks",
+    source: "https://raw.github.com/adrianmiu/callbacks/master/src/callbacks.js"
   }
 ];
