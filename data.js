0ada3927bdee2237554aab92d1f5d842295c9a1a
Gagan Bansal <gaganbansal123@gmail.com>
2015-04-06 07:21:29 -0400

    added geojson2svg

--- data.js
+++ data.js
@@ -9,6 +9,14 @@
 
 module.exports = [
   {
+    name: "geojson2svg",
+    github: "gagan-bansal/geojson2svg",
+    tags: ["maps", "geojson", "svg"],
+    description: "Converts geojson to svg string given svg viewport size and maps extent. Used for data visulizaton on maps.",
+    url: "https://github.com/gagan-bansal/geojson2svg",
+    source: "https://raw.githubusercontent.com/gagan-bansal/geojson2svg/master/dist/geojson2svg.js"
+  },
+  {
     name: "Timesheet.js",
     github: "sbstjn/timesheet.js",
     tags: ["timesheet", "graph", "charts", "flow", "gantt", "draw"],
