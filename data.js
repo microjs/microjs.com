c76ea734a8a90c39259c5be18b7f1b079676297e
Ahmed Musallam <atyaf66@gmail.com>
2018-02-03 10:24:22 -0600

    add pub-sub to list of files

--- data.js
+++ data.js
@@ -76,7 +76,7 @@ module.exports = [{
     tags: ["pubsub", "component-interaction", "event", "jquery"],
     description: "A tiny pub/sub jQuery micro-framework for distant component interaction",
     url: "https://github.com/ahmed-musallam/syrupjs",
-    source: "https://github.com/ahmed-musallam/syrupjs/blob/master/index.js"
+    source: ["https://github.com/ahmed-musallam/syrupjs/blob/master/vendor/tiny-pub-sub.js","https://github.com/ahmed-musallam/syrupjs/blob/master/index.js"]
   },
   {
     name: "blobcounter.js",
