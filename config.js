aba40c803acaded960bd6839f0d496b069e8b216
Sophie Kirschner <sophie@thehumangeo.com>
2018-05-04 10:36:32 +0300

    Edits to config to make limits slightly more permissive

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     inFile                     : 'data.js'
   , outFile                    : 'data-min.js'
     // for now; need to tweak this later
-  , githubLastPushCutoffMonths : 48
+  , githubLastPushCutoffMonths : 60
   , sizeExceptions             : [
         'Backbone'
       , 'Zepto'
@@ -11,12 +11,13 @@ module.exports = {
         'cm.js'
     ]
   , errorCountThreshold        : 10
-  , maxGzipped                 : 5 * 1024
+    // normal limit is 5kb, but strictly enforce 6kb
+  , maxGzipped                 : 6 * 1024
     // these 3 are basic sanity cecks, minimums that we would expect the contents
     // to be, otherwise something might be wrong
-  , minRaw                     : 300
-  , minMinified                : 150
-  , minGzipped                 : 100
+  , minRaw                     : 100
+  , minMinified                : 50
+  , minGzipped                 : 25
     // this is to compare raw with minified, if it's too close then we may have
     // been given the minified source in the first place
   , minifierTollerance         : 50
