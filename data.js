214610c4b77e6796d99caac1469e6eda4c892d33
Lars Kappert <lars@webpro.nl>
2018-08-14 08:30:49 +0200

    Fix source location of DOMtastic (#976)
    * Fix source location of DOMtastic
    
    * Update homepage url for DOMtastic

--- data.js
+++ data.js
@@ -4011,8 +4011,8 @@ module.exports = [{
     github: "webpro/DOMtastic",
     tags: ["DOM", "events", "selector", "modern", "modular", "es6"],
     description: "Small and fast DOM and event library for modern browsers. It has the same familiar API as jQuery, and is lean & mean with small, optional modules.",
-    url: "http://webpro.github.io/DOMtastic/",
-    source: "https://raw.githubusercontent.com/webpro/DOMtastic/gh-pages/dist/domtastic.js"
+    url: "https://domtastic.js.org",
+    source: "https://cdn.jsdelivr.net/npm/domtastic@latest/dist/domtastic.js"
   },
   {
     name: "Timer.js",
